e2b1c1d6850bb373c6ae06fb87b93a77098b10cb
dshank <shank.david@gmail.com>
2024-01-30 18:38:09 -0500

    Add handling for websocket dataframes, not including reading the payload
    Also corrects the max payload length to u64

--- src/websocket/mod.rs
+++ src/websocket/mod.rs
@@ -1,9 +1,70 @@
-use std::{error::Error, fmt::Display, net::TcpStream};
+use std::{error::Error, fmt::Display, io::Read, net::TcpStream};
 
 #[derive(Debug)]
-struct WebSocket {
+pub struct WebSocket {
     socket: TcpStream,
     awaiting_pong: bool,
+}
+
+impl WebSocket {
+    pub fn read_dataframe(&mut self) -> Result<DataFrame, WebSocketError> {
+        let mut header_bytes: [u8; 2] = [0; 2];
+        self.socket.read_exact(&mut header_bytes)?;
+
+        let byte = header_bytes[0];
+        let (fin, rsv1, rsv2, rsv3, opcode) = (
+            bit(byte, 7),
+            bit(byte, 6),
+            bit(byte, 5),
+            bit(byte, 4),
+            byte & 0x0F as u8,
+        );
+
+        // handle message length parsing
+        let (mask, payload_length) = (bit(header_bytes[1], 7) as bool, header_bytes[1] & 0x7F);
+        if !mask {
+            return Err(WebSocketError::UnencodedMessage);
+        }
+
+        let payload_length = match payload_length {
+            0..=125 => payload_length as u64,
+            126 => {
+                let mut length_bytes: [u8; 2] = [0; 2];
+                self.socket.read_exact(&mut length_bytes)?;
+                (length_bytes[0] as u64) << 8 + length_bytes[1] as u64
+            }
+            127 => {
+                let mut length_bytes: [u8; 8] = [0; 8];
+                self.socket.read_exact(&mut length_bytes)?;
+                // The most significant bit cannot be 1
+                if bit(length_bytes[0], 7) {
+                    return Err(WebSocketError::BadPayloadLength);
+                }
+                u64::from_be_bytes(length_bytes)
+            }
+            _ => panic!("Found a payload length value that is impossible"),
+        };
+
+        //TODO get the payload
+
+        Ok(DataFrame {
+            fin,
+            rsv1,
+            rsv2,
+            rsv3,
+            opcode: OpCode::try_from(opcode)?,
+            mask,
+            mask_key: 0,
+            payload_length,
+            payload: Vec::new(),
+        })
+    }
+}
+
+/// Gets the bit at position `position`. Positions are assumed to be big endian, so the 7th
+/// position is the most significant bit
+fn bit(byte: u8, position: u8) -> bool {
+    ((byte >> position) & 1) != 0
 }
 
 struct DataFrame {
@@ -13,8 +74,9 @@ struct DataFrame {
     rsv2: bool,
     rsv3: bool,
     opcode: OpCode,
+    mask: bool,
+    payload_length: u64,
     mask_key: u32,
-    payload_length: u128,
     payload: Vec<u8>,
 }
 
@@ -27,16 +89,16 @@ enum OpCode {
     Binary = 0x2,
 }
 
-impl TryFrom<u32> for OpCode {
+impl TryFrom<u8> for OpCode {
     type Error = WebSocketError;
 
-    fn try_from(value: u32) -> Result<Self, Self::Error> {
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             0x0 => Ok(OpCode::Continuation),
             0x1 => Ok(OpCode::Text),
             0x2 => Ok(OpCode::Binary),
             0x3..=0x7 => Err(WebSocketError::BadOpCode(value)),
-            0x10..=0xFF => Err(WebSocketError::BadOpCode(value)),   // Op codes are only 4 bits
+            0x10..=0xFF => Err(WebSocketError::BadOpCode(value)), // Op codes are only 4 bits
             opcode => Err(WebSocketError::OpCodeNotImplemented(opcode)),
         }
     }
@@ -44,8 +106,11 @@ impl TryFrom<u32> for OpCode {
 
 #[derive(Debug)]
 enum WebSocketError {
-    BadOpCode(u32),
-    OpCodeNotImplemented(u32),
+    BadOpCode(u8),
+    OpCodeNotImplemented(u8),
+    Io(std::io::Error),
+    UnencodedMessage,
+    BadPayloadLength,
 }
 
 impl Display for WebSocketError {
@@ -61,8 +126,17 @@ impl Display for WebSocketError {
                     opcode
                 )
             }
+            WebSocketError::Io(error) => error.fmt(f),
+            WebSocketError::UnencodedMessage => write!(f, "Mask bit set to 0"),
+            WebSocketError::BadPayloadLength => write!(f, "Payload length was > 2^63-1"),
         }
     }
 }
 
 impl Error for WebSocketError {}
+
+impl From<std::io::Error> for WebSocketError {
+    fn from(value: std::io::Error) -> Self {
+        WebSocketError::Io(value)
+    }
+}
